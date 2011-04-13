3309d0c9567b8bc391166e36d5f2430d0ab0edcc
ralfberge <ralfberge@me.com>
2011-04-14 00:03:22 +0200

    cpp umgesiedelt

---
+++ 24U Plug-In Template 3.0/Projects/XCode/cpp/orbitToolsDemo/stdafx.h
@@ -0,0 +1,16 @@
+//
+// stdafx.h 
+//
+#pragma once
+
+#define WIN32_LEAN_AND_MEAN   // Exclude rarely-used stuff from Windows headers
+#include <stdio.h>
+//#include <tchar.h>
+
+#include <string>
+#include <map>
+#include <vector>
+#include <algorithm>
+#include <assert.h>
+
+using namespace std;
